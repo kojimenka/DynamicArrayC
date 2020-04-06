28794f7db1b91ddeb0e175e94c95b4b5d9ba9a39
Dmitriy <demon2565@gmail.com>
2020-04-06 10:25:17 +0300

    Initial Commit

---
+++ DynamicArrat/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DynamicArrat
+//
+//  Created by Дмитрий Марченков on 06.04.2020.
+//  Copyright © 2020 Дмитрий Марченков. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
